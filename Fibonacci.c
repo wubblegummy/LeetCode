d6dba96f0f1766f6e0a6921e2c55a2ddfe483256
wubblegummy <maithrisent@gmail.com>
2025-10-02 11:54:15 +0530

    Added Fibonacci Program

---
+++ Fibonacci.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int fib(int n){
+    if (n==0)
+        return 0;
+    else if (n==1)
+        return 1;
+    else
+        return fib(n-1) + fib(n-2);
+}
+int main(){
+    int n;
+    printf("Enter the number");
+    scanf("%d",&n);
+    int result = fib(n);
+    printf("%d",result);
